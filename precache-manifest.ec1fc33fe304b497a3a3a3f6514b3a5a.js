6f933c3d69947dffe8de3eaa2b5f861bcebed94f
Peter J Tshiani <tshianipeter@gmail.com>
2020-03-17 05:43:00 +0200

    Updates

---
+++ precache-manifest.ec1fc33fe304b497a3a3a3f6514b3a5a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374bb14e39aaffca6f1919dbc0f08886",
+    "url": "/peterjtshiani/framework2index.html"
+  },
+  {
+    "revision": "f8965f401bb8f175cad2",
+    "url": "/peterjtshiani/framework2static/css/main.61f03028.chunk.css"
+  },
+  {
+    "revision": "752942806b3de77e08cd",
+    "url": "/peterjtshiani/framework2static/js/2.c04bde65.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/peterjtshiani/framework2static/js/2.c04bde65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8965f401bb8f175cad2",
+    "url": "/peterjtshiani/framework2static/js/main.32674ffb.chunk.js"
+  },
+  {
+    "revision": "60ae1f0395b96ab31da2",
+    "url": "/peterjtshiani/framework2static/js/runtime-main.e01da2ba.js"
+  },
+  {
+    "revision": "f2640f38e882dac8bf9ad7611bd4d769",
+    "url": "/peterjtshiani/framework2static/media/Cape-Town.f2640f38.jpg"
+  },
+  {
+    "revision": "2e592b083efb952e1866d4f6bebb1af9",
+    "url": "/peterjtshiani/framework2static/media/agent.2e592b08.jpg"
+  },
+  {
+    "revision": "b82d6103c0680ee07b745a6bbbfedb13",
+    "url": "/peterjtshiani/framework2static/media/bg.b82d6103.png"
+  },
+  {
+    "revision": "e219ec473f6747807e1ee7f8a5056788",
+    "url": "/peterjtshiani/framework2static/media/bg.e219ec47.jpg"
+  },
+  {
+    "revision": "d5fd7b482a5ff673f6a44ec743270293",
+    "url": "/peterjtshiani/framework2static/media/durban1.d5fd7b48.jpg"
+  },
+  {
+    "revision": "14766e88d4ef8f0d0e371068274e6ec4",
+    "url": "/peterjtshiani/framework2static/media/next.14766e88.jpg"
+  },
+  {
+    "revision": "14766e88d4ef8f0d0e371068274e6ec4",
+    "url": "/peterjtshiani/framework2static/media/pre.14766e88.jpg"
+  },
+  {
+    "revision": "75be9cfe1f0712970a7fa12579ccce53",
+    "url": "/peterjtshiani/framework2static/media/thumb.75be9cfe.jpg"
+  },
+  {
+    "revision": "c304a3a6b1dc0453151e668df29a142e",
+    "url": "/peterjtshiani/framework2static/media/thumb.c304a3a6.png"
+  },
+  {
+    "revision": "96e9027eb124f6bcbf2f1a267083ac6a",
+    "url": "/peterjtshiani/framework2static/media/thumb2.96e9027e.jpg"
+  }
+]);
